9c3201cd063d3d16cfab40fe4b0b87eaa7733b6c
ElliNet13 <elliotmoose4@gmail.com>
2026-02-05 18:47:03 -0700

    Place mines

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
+namespace SpriteKind {
+    export const Mine = SpriteKind.create()
+}
+function is_in_game_area (location: tiles.Location) {
+    if (location.column > 1 && location.column < 8) {
+        if (location.row > 1 && location.row < 6) {
+            return true
+        }
+    }
+    return false
+}
 function A () {
-    tiles.setTileAt(realPlayerSprite.tilemapLocation(), assets.tile`transparency16`)
+    tiles.setTileAt(visiblePlayerSprite.tilemapLocation(), assets.tile`Empty tile`)
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     B()
@@ -7,30 +18,48 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     A()
 })
+function place_mine () {
+    currentMine = sprites.create(assets.image`nothing`, SpriteKind.Mine)
+    tiles.placeOnRandomTile(currentMine, assets.tile`Covered tile`)
+    for (let value of sprites.allOfKind(SpriteKind.Mine)) {
+        if (currentMine.overlapsWith(value)) {
+            sprites.destroy(currentMine)
+            place_mine()
+        }
+    }
+}
 function B () {
-    tiles.setTileAt(realPlayerSprite.tilemapLocation(), assets.tile`flag`)
+    tiles.setTileAt(visiblePlayerSprite.tilemapLocation(), assets.tile`flag`)
 }
 browserEvents.onMouseMove(function (x, y) {
     realPlayerSprite.setPosition(x, y)
 })
+function place_mines () {
+    tiles.setCurrentTilemap(tilemap`Main screen`)
+    for (let index = 0; index < 4; index++) {
+        place_mine()
+    }
+}
 browserEvents.MouseLeft.onEvent(browserEvents.MouseButtonEvent.Pressed, function (x, y) {
     A()
 })
 browserEvents.MouseRight.onEvent(browserEvents.MouseButtonEvent.Pressed, function (x, y) {
     B()
 })
+let currentMine: Sprite = null
 let realPlayerSprite: Sprite = null
+let visiblePlayerSprite: Sprite = null
 scene.setBackgroundImage(assets.image`background`)
 tiles.setCurrentTilemap(tilemap`Main screen`)
-let visiblePlayerSprite = sprites.create(assets.image`player`, SpriteKind.Player)
+visiblePlayerSprite = sprites.create(assets.image`player`, SpriteKind.Player)
+tiles.placeOnTile(visiblePlayerSprite, tiles.getTileLocation(2, 2))
 realPlayerSprite = sprites.create(assets.image`realplayer`, SpriteKind.Player)
-tiles.placeOnTile(realPlayerSprite, tiles.getTileLocation(0, 0))
+tiles.placeOnTile(realPlayerSprite, tiles.getTileLocation(2, 2))
 controller.moveSprite(realPlayerSprite)
 browserEvents.setCursorVisible(false)
+place_mines()
 forever(function () {
-    if (realPlayerSprite.tilemapLocation().column > 1 && realPlayerSprite.tilemapLocation().column < 8) {
-        if (realPlayerSprite.tilemapLocation().row > 1 && realPlayerSprite.tilemapLocation().row < 6) {
-            tiles.placeOnTile(visiblePlayerSprite, realPlayerSprite.tilemapLocation())
-        }
+    if (is_in_game_area(realPlayerSprite.tilemapLocation())) {
+        tiles.placeOnTile(visiblePlayerSprite, realPlayerSprite.tilemapLocation())
     }
 })
